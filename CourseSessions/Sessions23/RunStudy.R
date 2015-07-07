400f608601f13a92421b032bb3f329e916b9c440
theodoros evgeniou <theos@theodoross-MacBook-Air.local>
2015-07-07 20:28:54 +0200

    minor

--- CourseSessions/Sessions23/RunStudy.R
+++ CourseSessions/Sessions23/RunStudy.R
@@ -22,7 +22,7 @@ local_directory <- paste(getwd(),"CourseSessions/Sessions23", sep="/")
 cat("\n *********\n WORKING DIRECTORY IS ", local_directory, "\n PLEASE CHANGE IT IF IT IS NOT CORRECT using setwd(..) - type help(setwd) for more information \n *********")
 
 # Please ENTER the name of the file with the data used. The file should contain a matrix with one row per observation (e.g. person) and one column per attribute. THE NAME OF THIS MATRIX NEEDS TO BE ProjectData (otherwise you will need to replace the name of the ProjectData variable below with whatever your variable name is, which you can see in your Workspace window after you load your file)
-datafile_name="MBAadmin" # do not add .csv at the end! make sure the data are numeric!!!! check your file!
+datafile_name="MBAadmin" #  do not add .csv at the end! make sure the data are numeric!!!! check your file!
 
 # Please ENTER the filename of the Report and Slides (in the doc directory) to generate 
 
