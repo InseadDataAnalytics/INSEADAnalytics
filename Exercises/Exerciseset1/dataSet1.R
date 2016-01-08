3caf376ca2bfa23f2233c735fcf9c0f1528cb614
theodoros evgeniou <theos@theodoross-MacBook-Air.local>
2016-01-08 12:34:32 +0100

    added some fun trading

--- Exercises/Exerciseset1/dataSet1.R
+++ Exercises/Exerciseset1/dataSet1.R
@@ -1,39 +1,39 @@
 
 # rm(list=ls()) # Clean up the memory, if we want to rerun from scratch
-# source("Exercises/Introduction to R/helpersSet1.R")
+# source("helpersSet1.R")
 
 getdata.fromscratch = 1
 
 website_used = "yahoo" # can be "yahoo" or other ( see help(getSymbols) ). Depending on the website we may need to change the stock tickers' representation
-
 mytickers = c("SPY", "AAPL")  # Other tickers for example are "GOOG", "GS", "TSLA", "FB", "MSFT", 
-NumberOfStocks = length(mytickers)
 startDate = "2005-01-01"
 
 if (getdata.fromscratch){
   # Get SPY first, to get all trading days
   tmp<-as.matrix(try(getSymbols(Symbols="SPY",from = startDate,src = website_used, auto.assign=FALSE)))
-  #cat(colnames(tmp))
-  
-  StockPrices=matrix(rep(NA,nrow(tmp)*NumberOfStocks), ncol=NumberOfStocks)
-  colnames(StockPrices)<-c(); 
+  StockPrices=matrix(rep(0,nrow(tmp)*length(mytickers)), ncol=length(mytickers))
+  colnames(StockPrices)<-mytickers; 
   rownames(StockPrices)<-rownames(tmp)
   StockVolume=StockPrices
   StockPrices[,1] <- tmp[,6]
   
-  ticker_to_get = mytickers[2]
-  tmpdata<-as.matrix(try(getSymbols(Symbols=ticker_to_get,from = startDate,auto.assign=FALSE)))
-  if (!inherits(tmpdata, "try-error"))
-  {
-    therownames=intersect(rownames(tmpdata),rownames(StockPrices))
-    StockPrices[therownames,2]<-tmpdata[therownames,6] # adjusted close price
-    StockVolume[therownames,2]<-tmpdata[therownames,5] # shares volume for now - need to convert to dollars later
-  } else {
-    cat(ticker_to_get," NOT found")
+  for (ticker_index in 1:length(mytickers)){
+    ticker_to_get = mytickers[ticker_index]
+    print(paste("\nDownloading ticker ", ticker_to_get, " ..."))
+    tmpdata<-as.matrix(try(getSymbols(Symbols=ticker_to_get,from = startDate,auto.assign=FALSE)))
+    if (!inherits(tmpdata, "try-error"))
+    {
+      therownames=intersect(rownames(tmpdata),rownames(StockPrices))
+      tmpdata[is.na(tmpdata)] <- 0
+      StockPrices[therownames,ticker_index]<-tmpdata[therownames,6] # adjusted close price
+      StockVolume[therownames,ticker_index]<-tmpdata[therownames,5] # shares volume for now - need to convert to dollars later
+    } else {
+      cat(ticker_to_get," NOT found")
+    }
   }
-  
   # Get the daily returns now. Use the simple percentage difference approach 
-  StockReturns= (tail(StockPrices,-1)-head(StockPrices,-1))/head(StockPrices,-1) # note that this removes the first day as we have no way to get the returns then!
+  StockReturns= ifelse(head(StockPrices,-1)!=0, (tail(StockPrices,-1)-head(StockPrices,-1))/head(StockPrices,-1),0) # note that this removes the first day as we have no way to get the returns then!
+  rownames(StockReturns)<-tail(rownames(StockPrices),-1) # adjust the dates by 1 day now
   
   # Now remove the first day from the other data, too
   StockPrices = StockPrices[rownames(StockReturns),]
