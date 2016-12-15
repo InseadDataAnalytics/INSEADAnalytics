54e81cf9af34dc091156c50bf63e90c4b39359bc
vkapartzianis <vkapartzianis@gmail.com>
2016-12-15 19:28:04 +0200

    Update list of required libraries

--- AnalyticsLibraries/library.R
+++ AnalyticsLibraries/library.R
@@ -1,23 +1,32 @@
 # Required R libraries (need to be installed - it can take a few minutes the first time you run the project)
 
-# installs all necessary libraries from CRAN
-get_libraries <- function(filenames_list) { 
-  lapply(filenames_list,function(thelibrary){    
-    if (do.call(require,list(thelibrary)) == FALSE) 
-      do.call(install.packages,list(thelibrary, repos="http://cran.r-project.org/")) 
-    do.call(library,list(thelibrary))
+
+# installs all necessary libraries from CRAN or Github
+get_libraries <- function(filenames_list) suppressPackageStartupMessages({ 
+  lapply(filenames_list, function(thelibrary){    
+    thelibrary.split <- strsplit(thelibrary, "/")[[1]]
+    if (length(thelibrary.split) > 1) {
+      # install from Github
+      if (!suppressWarnings(require(thelibrary.split[2], character.only=TRUE))) {
+        devtools::install_github(thelibrary, quiet=TRUE)
+        library(thelibrary.split[2], character.only=TRUE)
+      }
+    } else {
+      # install from CRAN
+      if (!suppressWarnings(require(thelibrary, character.only=TRUE))) {
+        install.packages(thelibrary, repos="http://cran.r-project.org/", quiet=TRUE)
+        library(thelibrary, character.only=TRUE)
+      }
+    }
   })
-}
+})
 
 libraries_used=c("devtools","knitr","graphics","grDevices","xtable","pryr",
                  "Hmisc","vegan","fpc","GPArotation","FactoMineR","cluster",
-                 "psych","stringr","googleVis", "png","ggplot2","googleVis", "gridExtra")
+                 "psych","stringr","googleVis", "png","ggplot2","googleVis", 
+                 "gridExtra", "reshape2", "ramnathv/slidifyLibraries",
+                 "ramnathv/slidify", "cttobin/ggthemr")
 get_libraries(libraries_used)
-
-if (require(slidifyLibraries) == FALSE) 
-  install_github("slidifyLibraries", "ramnathv")
-if (require(slidify) == FALSE) 
-  install_github("slidify", "ramnathv") 
 
 #############
 
