5ab95d5d5a15e119d09c3fcd1f8fc62ba5949e4f
theodoros.evgeniou@insead.edu <theodoros.evgeniou@insead.edu>
2016-12-30 09:31:08 +0200

    segmentation process

--- AnalyticsLibraries/library.R
+++ AnalyticsLibraries/library.R
@@ -26,7 +26,7 @@ libraries_used=c("devtools","knitr","graphics","grDevices","xtable","pryr",
                  "psych","stringr","googleVis", "png","ggplot2","googleVis", 
                  "gridExtra", "reshape2", "DT", "ramnathv/slidifyLibraries",
                  "ramnathv/slidify", "cttobin/ggthemr", "dplyr", "mrjoh3/c3",
-                 "rpart.plot","vkapartzianis/formattable", "ggdendro")
+                 "rpart.plot","vkapartzianis/formattable", "ggdendro","ROCR")
 get_libraries(libraries_used)
 
 #############
